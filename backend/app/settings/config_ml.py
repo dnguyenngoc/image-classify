b0069724bc325bb5285d5a55c642b4ee0f993c5b
Duy Nguyen <duynguyenngoc@hotmail.com>
2021-08-05 15:32:05 +0700

    fix train dât

--- backend/app/settings/config_ml.py
+++ backend/app/settings/config_ml.py
@@ -1,7 +1,7 @@
 
 
 LEN_FEATURE = 768
-LEN_TOKEN = 140
+LEN_TOKEN = 150
 INDEX_NAME = 'image-classify'
 ES_HOST = '10.1.32.130'
 ES_PORT = '9200'
@@ -9,11 +9,15 @@ ES_PORT = '9200'
 CLASSES = {
     1: 'discharge record',
     2: 'driver licence',
-    3: 'invoice',
+    # 3: 'invoice',
     4: 'resume',
     5: 'vehicle registration certificate',
     6: 'degree of bachelor',
     7: 'test'
 }
 
-STOPWORDS = set(['\\', '(', ')', ':', '.', ';', ',', '\\\\', '\\\\\\', '-', '%', '`', '—-', '?', '——', '--', '@',  '[', ']', '.....', '``', 'đụ', 'đéo'])
+STOPWORDS = set([
+    '\\', '(', ')', ':', '.', ';', ',', '\\\\', '\\\\\\', '-', '%', '`', '—-', '?', '——', '--', '@',  '[', ']', '.....', '``', 'đụ', 'đéo'
+    'cộng', 'hòa', 'xã', 'hội', 'chủ', 'nghĩa', 'việt', 'nam', 'độc', 'lập', 'tự', 'do', 'hạnh', 'phúc',
+    'hĩa', 'phỏ', 'chũ'
+])
